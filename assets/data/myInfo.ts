3d9a6d40efc6ba2c3e830454ed1592d2b61d2ccf
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-08-02 21:56:11 -0400

    Add files via upload

---
+++ assets/data/myInfo.ts
@@ -0,0 +1,10 @@
+import { Woohyu } from '../../app/woohyu';
+
+export const MYINFO : Woohyu = {
+    id: '991497524',
+    name: 'Hyun-Ju Woo',
+    login: 'woohyu',
+    campus: 'Trafalgar',
+    assignTitle: 'Angular More'
+
+}
